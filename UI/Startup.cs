e0be00853852c272ee2082e50089a042e4eeb578
SKarakayaa <sefakarakayaa@gmail.com>
2021-03-18 23:41:13 +0300

    update

--- UI/Startup.cs
+++ UI/Startup.cs
@@ -74,7 +74,7 @@ namespace UI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
