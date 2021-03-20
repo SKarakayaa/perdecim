f426d7db2b7d634e373029ec792979d41b002189
SKarakayaa <sefakarakayaa@gmail.com>
2021-03-20 22:29:05 +0300

    admin product create screen is added

--- UI/Controllers/ProductController.cs
+++ UI/Controllers/ProductController.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Core.Utilities.Results;
 using Entities.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UI.Controllers
@@ -15,10 +18,19 @@ namespace UI.Controllers
         }
         public async Task<IActionResult> Index(int id)
         {
-            string[] navigation = { "Category","ProductDemands","ProductImages","ProductDemands.DemandType","ProductDemands.DemandType.Demands" };
+            string[] navigation = { "Category", "ProductDemands", "ProductImages", "ProductDemands.DemandType", "ProductDemands.DemandType.Demands" };
             ViewBag.BaseUrl = "https://" + HttpContext.Request.Host.ToString();
             IDataResult<Product> product = await _productService.GetByIdAsync(id, navigation);
             return View(product.Data);
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public IActionResult Products()
+        {
+            string[] navigation = { "Category" };
+            IDataResult<List<Product>> products = _productService.GetListAsync(null, navigation).Result;
+            return View(products.Data);
         }
     }
 }
