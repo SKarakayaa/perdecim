0c3dd844ddac095b24622c534e3c559e2bd02d37
SKarakayaa <sefakarakayaa@gmail.com>
2021-05-17 20:39:41 +0300

    photos are added

--- UI/ViewComponents/HomeProducts.cs
+++ UI/ViewComponents/HomeProducts.cs
@@ -2,8 +2,10 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Entities.Concrete;
+using Entities.Config;
 using Entities.Enums;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace UI.ViewComponents
 {
@@ -12,9 +14,11 @@ namespace UI.ViewComponents
     public class HomeProducts : ViewComponent
     {
         private readonly IProductService _productService;
-        public HomeProducts(IProductService productService)
+        private readonly FileUploadSettings _fileUploadSettings;
+        public HomeProducts(IProductService productService, IOptions<FileUploadSettings> fileUploadSettings)
         {
             _productService = productService;
+            _fileUploadSettings = fileUploadSettings.Value;
         }
         public async Task<IViewComponentResult> InvokeAsync(int productType)
         {
@@ -22,21 +26,22 @@ namespace UI.ViewComponents
             switch (productType)
             {
                 case (int)ProductType.NEW_PRODUCT:
-                    products = (await _productService.GetListAsync(x => x.IsNew, x => x.Category, x => x.Brand)).Data;
+                    products = (await _productService.GetListAsync(x => x.IsNew, x => x.Category, x => x.Brand, x => x.ProductImages)).Data;
                     ViewBag.Title = "Yeni Ürünler";
                     break;
                 case (int)ProductType.DISCOUNTED_PRODUCT:
-                    products = (await _productService.GetListAsync(x => x.DiscountRate != 0, x => x.Category, x => x.Brand)).Data;
+                    products = (await _productService.GetListAsync(x => x.DiscountRate != 0, x => x.Category, x => x.Brand, x => x.ProductImages)).Data;
                     ViewBag.Title = "İndirimli Ürünler";
                     break;
                 case (int)ProductType.POPULAR_PRODUCT:
-                    products = (await _productService.GetListAsync(x => x.IsPopular, x => x.Category, x => x.Brand)).Data;
+                    products = (await _productService.GetListAsync(x => x.IsPopular, x => x.Category, x => x.Brand, x => x.ProductImages)).Data;
                     ViewBag.Title = "Popüler Ürünler";
                     break;
                 default:
                     products = (await _productService.GetListAsync()).Data;
                     break;
             }
+            ViewBag.ImageBaseUrl = $"{_fileUploadSettings.MainPath}{_fileUploadSettings.ProductImagePath}";
 
             return View(products);
         }
